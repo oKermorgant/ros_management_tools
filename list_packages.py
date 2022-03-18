a58a111e18cc9f94452585038c9d969b3633390e
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-03-18 14:31:44 +0100

    small tool to list manually installed ros packages

---
+++ list_packages.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+from subprocess import check_output
+import sys
+
+ros_prefix = 'ros-'
+if len(sys.argv) > 1 and not sys.argv[1].startswith('-'):
+    ros_prefix += sys.argv[1]    
+    
+single_row = '-r' in sys.argv
+    
+pkgs = check_output('apt list --installed' .split()).decode().splitlines()
+pkgs = [p.split('/')[0] for p in pkgs if p.startswith(ros_prefix) and ',automatic' not in p]
+
+if single_row:
+    print(' '.join(pkgs))
+else:
+    print('\n'.join(pkgs))
+
